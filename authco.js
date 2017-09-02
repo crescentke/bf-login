d014e68cc789ced4cb02a95c7bc7fc25965cf729
Crescent <hello@crescentke.com>
2017-09-02 08:18:32 +0300

    Updates

--- authco.js
+++ authco.js
@@ -8,24 +8,5 @@ module.exports = {
         'clientID'        : 'UOvghD62lX5b8DMXQdMzpRAAI', // your App ID
         'clientSecret'    : 'FKVXNDvfYjmqs0KJ657iiONd3nUPBFULRXXxjJeG8EqW83Rtbv', // your App Secret
         'callbackURL'     : 'https://localhost:8181/auth/facebook/callback'
-    },
-
-    'twitterAuth' : {
-        'consumerKey'        : 'xxxxxxxxxxxxxxxxxx',
-        'consumerSecret'     : 'xxxxxxxxxxxxxxxxxx',
-        'callbackURL'        : 'https://uourserver:8181/auth/twitter/callback'
-    },
-
-    'linkedinAuth' : {
-        'consumerKey'        : 'xxxxxxxxxxxxxxxxxx',
-        'consumerSecret'     : 'xxxxxxxxxxxxxxxxxx',
-        'callbackURL'        : 'https://yourserver:8181/auth/linkedin/callback'
-    },
-
-    'googleAuth' : {
-        'clientID'         : 'xxxxxxxxxxxxxxxxxx',
-        'clientSecret'     : 'xxxxxxxxxxxxxxxxxx',
-        'callbackURL'      : 'https://yourserver:8181/auth/google/callback'
     }
-
 };
